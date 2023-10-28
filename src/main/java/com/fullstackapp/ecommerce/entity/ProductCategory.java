269826741c3ab7b068f40a3737a377eb7dfdd71f
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-10-28 12:37:09 +0200

    add ProductCategory Entity and add relationship with Product Entity

---
+++ src/main/java/com/fullstackapp/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,24 @@
+package com.fullstackapp.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+// @Data (has bugs when using one-to-many and many-to-one relationships
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "category_name")
+    private String categoryName;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
