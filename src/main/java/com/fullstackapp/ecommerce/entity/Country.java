8237518b7f0cda7374d83291311d3823af893d74
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-11-17 10:47:57 +0100

    add country and state repositories

--- src/main/java/com/fullstackapp/ecommerce/entity/Country.java
+++ src/main/java/com/fullstackapp/ecommerce/entity/Country.java
@@ -1,5 +1,6 @@
 package com.fullstackapp.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,5 +23,6 @@ public class Country {
     private String name;
 
     @OneToMany(mappedBy = "country")
+    @JsonIgnore
     private List<State> states;
 }
