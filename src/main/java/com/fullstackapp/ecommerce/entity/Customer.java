7578cc0f5f8cefe0c4a11af67e09ee0a5cf7d1aa
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-11-24 12:08:08 +0100

    add relationships between Order, OrderItem, Address and Customer

--- src/main/java/com/fullstackapp/ecommerce/entity/Customer.java
+++ src/main/java/com/fullstackapp/ecommerce/entity/Customer.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "customer")
 @Getter
@@ -20,4 +23,17 @@ public class Customer {
     private String lastName;
     @Column(name = "email")
     private String email;
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private Set<Order> orders = new HashSet<Order>();
+
+    public void add(Order order) {
+        if (order != null) {
+            if (orders == null) {
+                orders = new HashSet<>();
+            }
+
+            orders.add(order);
+            order.setCustomer(this);
+        }
+    }
 }
--- src/main/java/com/fullstackapp/ecommerce/entity/Order.java
+++ src/main/java/com/fullstackapp/ecommerce/entity/Order.java
@@ -8,6 +8,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "orders")
@@ -40,4 +42,31 @@ public class Order {
     @UpdateTimestamp
     private Date lastUpdated;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "order")
+    private Set<OrderItem> orderItems = new HashSet<OrderItem>();
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "shipping_address_id", referencedColumnName = "id")
+    private Address shippingAddress;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "billing_address_id", referencedColumnName = "id")
+    private Address billingAddress;
+
+
+
+    public void add(OrderItem item) {
+        if (item != null) {
+            if (orderItems == null) {
+                orderItems = new HashSet<OrderItem>();
+            }
+
+            orderItems.add(item);
+            item.setOrder(this);
+        }
+    }
 }
