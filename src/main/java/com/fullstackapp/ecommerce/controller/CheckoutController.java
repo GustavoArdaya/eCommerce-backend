c600f195f089ba6f88b97d3b262e2400abf6f4d9
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-11-25 13:47:38 +0100

    add placeOrder method and CheckoutController

---
+++ src/main/java/com/fullstackapp/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,25 @@
+package com.fullstackapp.ecommerce.controller;
+
+import com.fullstackapp.ecommerce.dto.Purchase;
+import com.fullstackapp.ecommerce.dto.PurchaseResponse;
+import com.fullstackapp.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}
