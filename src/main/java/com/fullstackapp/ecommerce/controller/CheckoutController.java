38bde4be11b5d569916c338be74c781fbecd8e41
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-12-01 09:41:54 +0100

    add MyAppConfig. Add method to configure cors for Controllers. Remove CrossOrigins annotation from CheckoutController

--- src/main/java/com/fullstackapp/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/fullstackapp/ecommerce/controller/CheckoutController.java
@@ -5,7 +5,6 @@ import com.fullstackapp.ecommerce.dto.PurchaseResponse;
 import com.fullstackapp.ecommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
