08de217757a9ffea8895fd02ba4fbe9425d3bb4f
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-12-01 09:35:48 +0100

    refactor hard-coded cors configuration in configureRepositoryRestConfiguration to use variables

--- src/main/java/com/fullstackapp/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/fullstackapp/ecommerce/config/MyDataRestConfig.java
@@ -7,6 +7,7 @@ import com.fullstackapp.ecommerce.entity.State;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -19,6 +20,9 @@ import java.util.Set;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Value("${allowed.origins}")
+    private String[] allowedOrigins;
 
     private EntityManager entityManager;
 
@@ -42,7 +46,8 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         // call internal helper method to expose Ids
         exposeIds(config);
 
-        cors.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+        // configure CORS mapping
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(allowedOrigins);
     }
 
     private static void disableHttpMethods(Class theClass,RepositoryRestConfiguration config, HttpMethod[] unsupportedActions) {
